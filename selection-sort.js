652f2a8a7e866bd78687a1e499e3a388f9e478ec
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-05-13 12:08:15 +0200

    selection sort algorithm

--- selection-sort.js
+++ selection-sort.js
@@ -1,55 +1,64 @@
-/*
-* selection sort with 2 for loops
-*/
+/**
+ * Selection sort - using 2 loops
+ * 
+ * @param {array} arr - random
+ * 
+ * @returns {array} arr - sorted
+ */
 function selectionSort1(arr){
-  let min;
-  const len = arr.length;
+    let min;
 
-  for(let i = 0; i < len; i++){
-    min = i;
-    for(let j = i + 1; j < len; j++){
-       if(arr[j] < arr[min]){
-          min = j;
-       }
+    for(let i = 0; i < arr.length; i++){
+        min = i;
+        for(let j = i + 1; j < arr.length; j++){
+            if(arr[j] < arr[min]){
+                min = j;
+            }
+        }
+        swap(arr, i, min);
     }
-    swap(arr, i, min);
-  }
-  return arr;
+
+    return arr;
 }
 
-/*
-* selection sort recursive
-*/
+
+/**
+ * Selection sort - recursive
+ * 
+ * @param {array} arr - random
+ * @param {number} startIndex
+ * 
+ * @returns {array} arr - sorted
+ */
 function selectionSort2(arr, startIndex) {
-  let minIndex;
-  const len = arr.length;
+    let minIndex;
+    startIndex = startIndex ?? 0; // If undefined, set it to 0
 
-  // if startIndex is not defined, set it to 0
-  if (startIndex == null) { startIndex = 0 }
-
-  /*
-  * while startIndex is lower than array length continue looping
-  * if current iteration lower array value lower than lowest value
-  * swap lowest value with startIndex value -> lowest value goes to the beginning of the array
-  */
-  while (startIndex < len - 1) {
-    minIndex = startIndex;
-    for (let i = startIndex + 1; i < len; i++) {
-      if(arr[i] < arr[minIndex]) {
-        minIndex = i;
-      }
+    while (startIndex < arr.length - 1) {
+        minIndex = startIndex;
+        for (let i = startIndex + 1; i < arr.length; i++) {
+            if(arr[i] < arr[minIndex]) {
+                minIndex = i;
+            }
+        }
+        swap(arr, startIndex, minIndex);
+        startIndex++;
+        selectionSort2(arr, startIndex);
     }
-    swap(arr, startIndex, minIndex);
-    startIndex++;
-    selectionSort2(arr, startIndex);
-  }
 
-  return arr;
+    return arr;
 }
 
-// helper function swap
-function swap(arr, i, min) {
-  var temp = arr[i];
-  arr[i] = arr[min];
-  arr[min] = temp;
+
+/**
+ * Swap two elements in array
+ * 
+ * @param {arr} arr 
+ * @param {number} a
+ * @param {number} b 
+ */
+function swap(arr, a, b) {
+    let temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
 }
