5cec4f6df5b582803b19329bbf458f27ceedc8f1
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-05-13 11:27:01 +0200

    insertion sort algorithm

--- insertion-sort.js
+++ insertion-sort.js
@@ -6,6 +6,9 @@
  * @returns {array} arr - sorted
  */
 function insertionSort(arr) {
+
+    // If only one element in the array
+    if (arr.length < 2) return arr;
     
     arr.map( (el, i) => {
         while(i > 0 && arr[i - 1] > el){
