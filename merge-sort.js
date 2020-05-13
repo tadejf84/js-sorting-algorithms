441fad3462ce41150fe7a695ab20a0546da6d77b
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-05-13 11:25:54 +0200

    merge sort algorithm

--- merge-sort.js
+++ merge-sort.js
@@ -1,50 +1,51 @@
-/*
-* merge sort array
-* merge Sort is a Divide and Conquer algorithm. 
-* divides input array in 2 halves, calls itself for both and merges again
-*/
+/**
+ * Merge sort algorithm - a Divide and Conquer algorithm
+ * Divides array in 2 halves, recursively does this until array is sorted
+ * 
+ * @param {array} arr - random
+ * 
+ * @returns {array} arr - sorted
+ */
 function mergeSort (arr) {
 
-  // filter number elements only
-  const arrNumOnly = arr.filter(function(item){
-    return typeof item === 'number';
-  });
+    // If only one element in the array
+    if (arr.length < 2) return arr;
 
-  // if only one element in the array
-  if (arrNumOnly.length === 1) {
-    return arrNumOnly;
-  }
+    // Split array in two halves
+    const middle = Math.floor(arr.length / 2); // Get middle point in array
+    const left = arr.slice(0, middle);
+    const right = arr.slice(middle, arr.length);
 
-  // split array in two halves
-  const middle = Math.floor(arrNumOnly.length / 2);
-  const left = arrNumOnly.slice(0, middle);
-  const right = arrNumOnly.slice(middle, arrNumOnly.length);
-
-  // merge both halves
-  return merge(mergeSort(left), mergeSort(right));
+    // Merge both halves
+    return merge(mergeSort(left), mergeSort(right));
 }
 
 
-/*
-* helper function to merge left and right halves
-*/
+/**
+ * Merge left and right halves
+ * 
+ * @param {array} left 
+ * @param {array} right 
+ * 
+ * @returns {array} output
+ */
 function merge(left, right) {
-  let output = [],
-      indexLeft = 0,
-      indexRight = 0;;
+    let output = [],
+        indexLeft = 0,
+        indexRight = 0;;
 
-  // loop throught each element in left and right array and compare
-  while (indexLeft < left.length && indexRight < right.length ) {
-    if (left[indexLeft] < right[indexRight]) {
-      output.push(left[indexLeft]);
-      indexLeft++;
-    } else {
-      output.push(right[indexRight]);
-      indexRight++;
+    // Loop throught each element in left and right array and compare
+    while (indexLeft < left.length && indexRight < right.length ) {
+        if (left[indexLeft] < right[indexRight]) {
+            output.push(left[indexLeft]);
+            indexLeft++;
+        } else {
+            output.push(right[indexRight]);
+            indexRight++;
+        }
     }
-  }
 
-  // concatenate left and right arrays to the output array
-  return output.concat(left.slice(indexLeft)).concat(right.slice(indexRight));
+    // Concatenate left and right arrays to the output array
+    return output.concat(left.slice(indexLeft)).concat(right.slice(indexRight));
 }
 
